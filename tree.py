721fe7d441b519618b4a91720633b96db5239bf4
ibtkm <ibtkm2009@gmail.com>
2014-11-28 17:06:18 +0330

    QuadTree data structure added

---
+++ tree.py
@@ -0,0 +1,47 @@
+import os
+
+class QuadTree:
+	def __init__(self, max_height):
+		self.storage_size = (4**(max_height+1) - 1) / 3
+		self.storage = []
+		self.max_nodes = self.storage_size
+		self.max_height = max_height
+		self.root_node = 0
+		self.current_node = 0
+		self.load_storage()
+
+	def load_storage(self):
+		for h in range(0, self.max_height + 1):
+				for y in range(0, 2 ** h):
+					for z in range(0, 2 ** h):
+						self.storage.append([h,y,z])
+	
+	def parent(self):
+		return self.current_node / 4
+	def first_c(self):
+		return self.current_node * 4 + 1
+	def second_c(self):
+		return self.current_node * 4 + 2
+	def third_c(self):
+		return self.current_node * 4 + 3
+	def fourth_c(self):
+		return self.current_node * 4 + 4
+
+	def current_value():
+		return self.storage[self.current_node]
+
+	def value(self, id):
+		return self.storage[id]
+
+	def move_parent(self):
+		self.current_node = parent()
+	def move_first(self):
+		self.current_node = first_c()
+	def move_second(self):
+		self.current_node = second_c()
+	def move_third(self):
+		self.current_node = third_c()
+	def move_fourth(self):
+		self.current_node = fourth_c()
+	
+QuadTree(3)
